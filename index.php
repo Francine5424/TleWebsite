9012bb3c977b783ff5ee56f2fd8ff6e81aa414d6
Francine5424 <77037619+Francine5424@users.noreply.github.com>
2021-02-08 12:52:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,14 +15,14 @@
   <br>
   <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
   <font face="TIMES NEW ROMAN" <size="8" color="#228B22" <u><b><i>HOMEPAGE</i></b></font> 
-  <h3 align="center" <h1> <font face="broadway" color="#228B22" size="12"> <u><b> Fight For Future </b></u> </font></h3>
+  <h3 align="center" <h1> <font face="broadway" color="#228B22" size="13"> <u><b> Fight For Future </b></u> </font></h3>
   <font face="broadway" color="#228B22" size="12"> <h3 align="center"> <font face="broadway" color="#228B22" size="5"> TODAY VS. TOMMOROW </font> <p align="center"> <img src="120560213_336805987578720_8788332043839710527_n.jpg" alt="" width="700" length="500"> </p> <br><br><br> <p align="center"> 
      <marquee behavior="scroll" direction="left" scrolldelay="800" scrollamount="100"> 
       <font face="broadway" color="Yellow" size="8"> <u>Warning!!!</u> </font> 
      </marquee> 
      <marquee behavior="scroll" direction="left" scrolldelay="800" scrollamount="100" <p align="center"> 
-      <img src="pandemic.jpeg" alt="" length="200" width="200"> 
-      <img src="danger.jpeg" alt="" length="200" width="200"> 
+      <img src="148272549_3528561177197205_4193779885277393425_n.jpg" alt="" length="200" width="200"> 
+      <img src="91291664_497148214503160_784467616176537600_n.jpg" alt="" length="200" width="200"> 
       <p></p> 
      </marquee> </p></h3></font>
  
