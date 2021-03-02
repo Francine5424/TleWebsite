dea6bad421272fde55fcffd96064afda91dc6394
Francine5424 <77037619+Francine5424@users.noreply.github.com>
2021-03-02 19:25:18 +0800

    Update and rename TLE-scaff-3 (1).html to TLE-scaff-3 (1).php

---
+++ TLE-scaff-3 (1).php
@@ -0,0 +1,48 @@
+<html> 
+<head>
+ <title> FIGHT FOR FUTURE </title>
+</head><body background="heaven.jpeg">
+
+<font face="Algerian" ,="" color="light green" size="17">
+<marquee direction="left"> NEVER LOSING HOPE </marquee></font>
+<font face="Algerian" ,="" color="green" size="20">
+<marquee direction="right"> AGAINST COVID-19 </marquee></font>
+<center>
+ 
+<font face="Berlin Sans FB " ,="" color="#A52A2A" size="8">
+<h1> TODAY </h1>
+</font>
+<font face="Bahnschrift SemiBold" ,="" color="2F4F4F" size="4">
+<b>Today we faced so many problem.</b> <br>
+<b>And one the biggest problem that most of the people facing is the Pandemic.</b><br> 
+<b> But never lose hope because every problem has a solution.</b><br><br><br><p>
+
+<p align="center">
+<b> This event leaves a big impact to all, until now. This virus is the reason why we are in this situation right now. </b><br>
+<b> I think this Pandemic will leave us a memorable nightmare.</b><br>
+<b>But the pandemic teaches us new way of learning without going to school. And this pandemic teaches us to clean our surrounding <br><br> 
+</b></font><b>
+ 
+<audio controls="">
+ <p align="left">
+  <source src="Rachel Platten - Fight Song (Official Video)(MP3_70K).mp3" type="audio/mpeg"> 
+ </audio>
+<p align="center">
+<img src="dove2.jpeg",="" length="500" width="500"><br><br><br>
+<font face=" Bahnschrift SemiBold="" color="orange" size="6">
+<b> <i>“Hope is being able to see that there is light despite all of the darkness.”</i><br> 
+<i> -Desmond Tutu </i> <br><br><br><br> 
+</b></font></b></center>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
